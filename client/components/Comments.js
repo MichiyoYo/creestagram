bb850830a83f1e391c7948aff710bae28349b3b6
Cristina Lester <cristina.deelester@gmail.com>
2021-10-05 16:49:43 -0700

    added remove comment func

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,20 +1,29 @@
 import React from "react";
 
 const Comments = React.createClass({
-  renderComment(comment, index) {
+  renderComment(comment, i) {
     return (
-      <div className="comment" key={index}>
+      <div className="comment" key={i}>
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button
+            className="remove-comment"
+            onClick={this.props.removeComment.bind(
+              null,
+              this.props.params.postId,
+              i
+            )}
+          >
+            &times;
+          </button>
         </p>
       </div>
     );
   },
   handleSubmit(e) {
     e.preventDefault();
-    const { postId } = this.props;
+    const { postId } = this.props.params;
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
     this.props.addComment(postId, author, comment);
